f43ec1e9b4cb2a1d3910f06463920a65e05cc903
Akhilesh <akhilesh.patel@tarento.com>
2021-07-07 17:22:18 +0530

    Refactor Code

---
+++ app/services/ongoing_wms/article/create_or_update.rb
@@ -0,0 +1,126 @@
+module OngoingWms
+  module Article
+    class CreateOrUpdate < ApplicationService
+      attr_reader :article, :distributor, :goods_owner_id
+
+      def initialize(args = {})
+        super
+        @distributor = args[:distributor]
+        @article = args[:article]
+        @goods_owner_id = args[:goods_owner_id]
+      end
+
+      def call
+        begin
+          create_or_update_article(article_data)
+        rescue ServiceError => error
+          add_to_errors(error.messages)
+        end
+
+        completed_without_errors?
+      end
+
+      private
+
+      def create_or_update_article(article_data)
+        response = SpreeOngoingWms::Api.new(@distributor).create_or_update_article(article_data)
+        if response.success?
+          response = JSON.parse(response.body, symbolize_names: true)
+          puts response
+        else
+          raise ServiceError.new([Spree.t(:error, response: response)])
+        end
+      end
+
+      def article_data
+        {
+          goodsOwnerId: @goods_owner_id,
+          articleNumber: @article.id,
+          # articleGroup: {
+          #   code: "aliquip",
+          #   name: "aliqua laboris commodo"
+          # },
+          articleCategory: {
+            code: @article.category&.permalink,
+            name: @article.category&.name
+          },
+          # articleColor: {
+          #   code: "in labore nostrud Lorem",
+          #   name: "nulla"
+          # },
+          # articleSize: {
+          #   code: "aliquip ad labore",
+          #   name: "sunt veniam voluptate nisi laboris"
+          # },
+          articleName: @article.name,
+          # productCode: "<string>",
+          # unitCode: "<string>",
+          description: @article.description,
+          # isStockArticle: "<boolean>",
+          supplierInfo: {
+            # supplierArticleNumber: "<string>",
+            # supplierNumber: "<string>",
+            supplierName: @article.vendor&.name
+          },
+          barCodeInfo: {
+            barCode: @article.sku,
+            # barCodePackage: "<string>",
+            # barCodePallet: "<string>",
+            # alternativeBarCodes: [
+            #   {
+            #     barCode: "<string>",
+            #     quantityPerBarCode: "<decimal>",
+            #     barCodeType: {
+            #       value: "<Error: Too many levels of nesting to fake this schema>"
+            #     }
+            #   },
+            #   {
+            #     barCode: "<string>",
+            #     quantityPerBarCode: "<decimal>",
+            #     barCodeType: {
+            #       value: "<Error: Too many levels of nesting to fake this schema>"
+            #     }
+            #   }
+            # ]
+          },
+          # quantityPerPackage: "<integer>",
+          # quantityPerPallet: "<integer>",
+          weight: @article.weight,
+          # length: "<decimal>",
+          width: @article.width,
+          height: @article.height,
+          # volume: "<decimal>",
+          # purchasePrice: "<decimal>",
+          # stockValuationPrice: "<decimal>",
+          customerPrice: @article.price,
+          purcaseCurrencyCode: @article.cost_currency,
+          # countryOfOriginCode: "<string>",
+          # statisticsNumber: "<string>",
+          # articleNameTranslations: [
+          #   {
+          #     languageCode: "<string>",
+          #     articleName: "<string>"
+          #   },
+          #   {
+          #     languageCode: "<string>",
+          #     articleName: "<string>"
+          #   }
+          # ],
+          # stockLimit: "<integer>",
+          # minimumReorderQuantity: "<decimal>",
+          netWeight: @article.weight,
+          # linkToPicture: "<string>",
+          # structureDefinition: {
+          #   articleKind: "<string>",
+          #   rows: [
+          #     {
+          #       articleNumber: "<string>",
+          #       numberOfItems: "<decimal>"
+          #     }
+          #   ]
+          # }
+        }.to_json
+      end
+    end
+  end
+end
---
+++ app/services/ongoing_wms/order/create_or_update.rb
@@ -0,0 +1,164 @@
+module OngoingWms
+  module Order
+    class CreateOrUpdate < ApplicationService
+      attr_reader :order, :distributor, :goods_owner_id
+
+      def initialize(args = {})
+        super
+        @distributor = args[:distributor]
+        @order = args[:order]
+        @goods_owner_id = args[:goods_owner_id]
+      end
+
+      def call
+        begin
+          create_or_update_order(order_data)
+        rescue ServiceError => error
+          add_to_errors(error.messages)
+        end
+
+        completed_without_errors?
+      end
+
+      private
+
+      def create_or_update_order(order_data)
+        response = SpreeOngoingWms::Api.new(@distributor).create_or_update_order(order_data)
+        if response.success?
+          response = JSON.parse(response.body, symbolize_names: true)
+          puts response
+        else
+          raise ServiceError.new([Spree.t(:error, response: response)])
+        end
+      end
+
+      def order_data
+        {
+          goodsOwnerId: @goods_owner_id,
+          orderNumber: @order.number,
+          deliveryDate: 2.days.from_now,
+          consignee: consignee_detail,
+          # referenceNumber: "<string>",
+          # goodsOwnerOrderId: "<string>",
+          # salesCode: "<string>",
+          # orderRemark: "<string>",
+          # deliveryInstruction: "<string>",
+          # servicePointCode: "<string>",
+          # freeText1: "<string>",
+          # freeText2: "<string>",
+          # freeText3: "<string>",
+          orderType: {
+            code: "sunt elit",
+            name: "tempor"
+          },
+          wayOfDelivery: {
+            code: "veniam id officia tempor Ut",
+            name: "nulla elit am"
+          },
+          emailNotification: {
+            toBeNotified: false,
+            # value: "<string>"
+          },
+          smsNotification: {
+            toBeNotified: false,
+            # value: "<string>"
+          },
+          telephoneNotification: {
+            toBeNotified: false,
+            # value: "<string>"
+          },
+          transporter: {
+            # transporterCode: "<string>",
+            # transporterServiceCode: "<string>"
+          },
+          returnTransporter: {
+            # transporterCode: "<string>",
+            # transporterServiceCode: "<string>"
+          },
+          orderLines: order_lines,
+          customsInfo: {
+            # customsValueCurrencyCode: "<string>"
+          },
+          # preparedTransportDocumentId: "<string>",
+          # freightPrice: "<decimal>",
+          # warehouseId: "<integer>",
+          # classes: [
+          #   {
+          #     code: "<string>",
+          #     name: "<string>",
+          #     comment: "<string>"
+          #   },
+          #   {
+          #     code: "<string>",
+          #     name: "<string>",
+          #     comment: "<string>"
+          #   }
+          # ],
+          termsOfDeliveryType: {
+            code: "ut en",
+            name: "Duis proident velit"
+          },
+          customerPrice: @order.total
+        }.to_json
+      end
+
+      def order_lines
+        # [
+        #   {
+        #     rowNumber: "<string>",
+        #     articleNumber: "<string>",
+        #     numberOfItems: "<decimal>",
+        #     comment: "<string>",
+        #     shouldBePicked: "<boolean>",
+        #     serialNumber: "<string>",
+        #     lineTotalCustomsValue: "<decimal>",
+        #     batchNumber: "<string>",
+        #     lineType: {
+        #       code: "labore Lorem",
+        #       name: "exercitation aliqua dolore"
+        #     }
+        #   },
+        #   {
+        #     rowNumber: "<string>",
+        #     articleNumber: "<string>",
+        #     numberOfItems: "<decimal>",
+        #     comment: "<string>",
+        #     shouldBePicked: "<boolean>",
+        #     serialNumber: "<string>",
+        #     lineTotalCustomsValue: "<decimal>",
+        #     batchNumber: "<string>",
+        #     lineType: {
+        #       code: "eu nostrud ullamco ut",
+        #       name: "fugiat deserunt "
+        #     }
+        #   }
+        # ]
+        line_items = []
+        @order.line_items.each do |item|
+          line_item = {}
+          line_item[:rowNumber] = item.id
+          line_item[:articleNumber] = item.product.id
+          line_item[:numberOfItems] = item.quantity
+          line_items << line_item
+        end
+        line_items
+      end
+
+      def consignee_detail
+        {
+          customerNumber: @order.ship_address.phone,
+          name: @order.ship_address.first_name + ' ' + @order.ship_address.last_name,
+          address1: @order.ship_address.address1,
+          address2: @order.ship_address.address2,
+          # address3: "<string>",
+          postCode: @order.ship_address.zipcode,
+          city: @order.ship_address.city,
+          countryCode: @order.ship_address.country.iso,
+          # countryStateCode: "<string>",
+          # remark: "<string>",
+          # doorCode: "<string>"
+        }
+      end
+    end
+  end
+end
